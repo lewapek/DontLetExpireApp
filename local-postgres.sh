33c5f1c9a80e69a79bf35c9ca37f1ebe304c26a0
Paweł Kołodziejczyk <lewapek@gmail.com>
2023-01-23 15:39:47 +0100

    reformat local-postgres script

--- local-postgres.sh
+++ local-postgres.sh
@@ -1,6 +1,6 @@
 if [ $# -ne 1 ]; then
-	echo "One argument required"
-	exit 1
+  echo "One argument required"
+  exit 1
 fi
 
 container_name=local-postgres
@@ -9,7 +9,7 @@ pass=default
 db_name=dontletexpiredb
 
 case $1 in
-	"r" | "restart" )
+  "r" | "restart" )
     echo "Restarting postgres"
     docker rm -f ${container_name} 2>/dev/null && \
     docker run --name ${container_name} -d --restart unless-stopped --memory "1.5g" --cpus "1.0" -p 5432:5432 \
@@ -18,17 +18,17 @@ case $1 in
      -e POSTGRES_DB=${db_name} \
      postgres:14-alpine \
      postgres -c log_statement=all
-	  ;;
-	"e" | "exec" )
-	  echo "Executing into postgres"
-	  docker exec -it ${container_name} psql -U ${user} -d ${db_name}
-	  ;;
-	"logs" )
-	  echo "Showing logs"
-	  docker logs ${container_name} -f
-	  ;;
-	* )
-	  echo "Incorrect argument"
-	  exit 2
-	  ;;
+    ;;
+  "e" | "exec" )
+    echo "Executing into postgres"
+    docker exec -it ${container_name} psql -U ${user} -d ${db_name}
+    ;;
+  "logs" )
+    echo "Showing logs"
+    docker logs ${container_name} -f
+    ;;
+  * )
+    echo "Incorrect argument"
+    exit 2
+    ;;
 esac
